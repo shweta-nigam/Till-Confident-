d18a2d10ed5f2657a78e793fa84447ccb19b7e33
Shweta Nigam <shwetanigam2106@gmail.com>
2025-11-09 12:52:37 +0530

    practice string and integers

---
+++ 01_JS/01_JS_basics_logic/01_practice.js
@@ -0,0 +1,7 @@
+
+
+// Addition and concatenate with sum arithmetic operator
+
+console.log(3 + 2 +" is the sum of 3 and 2")          // 5 is the sum of 3 and 2
+console.log("sum of 10 and 20 here is " + 10 + 20)     // concatenate of 10 and 20 here is 1020
+console.log("sum of 10 and 20 here is " + (10 + 20))     // sum of 10 and 20 here is 1020
