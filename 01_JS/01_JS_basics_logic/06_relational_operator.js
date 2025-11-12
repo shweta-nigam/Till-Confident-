797049bd1b95a9f6d2526c32a98d7555a14c30f6
Shweta Nigam <shwetanigam2106@gmail.com>
2025-11-12 20:26:21 +0530

    revise relational operator

---
+++ 01_JS/01_JS_basics_logic/06_relational_operator.js
@@ -0,0 +1,11 @@
+// Operational operator
+// < > <= >= != = == ===
+
+console.log(10 != 10); // false
+console.log(10 !== 10); // false  (strict not equal)
+console.log(10 !== 11); //
+
+// ------------
+// = assignment operator - for assignment 
+// ==  equal 
+// === strict equal 
