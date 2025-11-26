43d59b97f52f332be68b37c90a4dfc022d6e4772
Shweta Nigam <shwetanigam2106@gmail.com>
2025-11-26 16:37:13 +0530

    find prime number using loop in optimized way

--- 01_JS/03_loop/02_pratice.js
+++ 01_JS/03_loop/02_pratice.js
@@ -76,3 +76,50 @@ console.log(a);
 
 // As any number divided by 2 would make the numbers above the divided outcome to be non-factor except the number itself.
 // ex: 8 , 8/2 = 4 , then all factor will be <= 4 and 8 itself(which is >=+- 8).
+
+//----------------------------------------------------------
+// prime number
+// prime number is the number which is divisible by 1 and itself.
+// 2 is only even prime number.
+// why 1 is not a prime number ?
+// Because :- The number 1 is not a prime number because it only has one factor, itself, while a prime number must have exactly two distinct positive factors: 1 and itself.
+
+// check prime number
+let b = 19;
+let isPrime = true;
+for (let i = 2; i < b / 2; i++) {
+  if (b % i === 0) {
+    isPrime = false;
+    break;
+  }
+}
+console.log(isPrime);
+
+//-----------------------------------------------------
+// very optimized way to find prime number
+
+if (ans === null || ans === undefined) {
+  console.log("cancelled");
+} else {
+  let n = Number(ans);
+  if (isNaN(n)) {
+    console.log("Invalid input");
+  } else {
+    if (n > 0) {
+      isPrime(n);
+    } else {
+      console.log("should be +ve and more than 0 ");
+    }
+  }
+}
+
+function isPrime(n) {
+  if (n <= 1) return false;
+  if (n === 2) return true; // 2 is a prime number
+  if (n % 2 === 0) return false;
+  for (let i = 3; i < Math.floor(Math.sqrt(n)); i += 2) {
+    // i+=2 because no even number is a prime number except 2
+    if (n % i === 0) return false;
+  }
+  return true;
+}
