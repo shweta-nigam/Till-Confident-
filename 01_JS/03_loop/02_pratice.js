b4984e42703b2d37835133df1e785f245f91ab53
Shweta Nigam <shwetanigam2106@gmail.com>
2025-11-26 15:57:56 +0530

    find factor and factorial  using loop

--- 01_JS/03_loop/02_pratice.js
+++ 01_JS/03_loop/02_pratice.js
@@ -19,3 +19,60 @@ if (num > 0) {
 } else {
   console.log("number should be +ve and more than 0");
 }
+
+//----------------------------------------------------
+// factorial of n numbers
+
+let n = 4;
+let factorial = 1;
+for (let i = n; i > 0; i--) {
+  factorial *= i;
+  // console.log(factorial);
+}
+console.log(factorial);
+
+//
+let n1 = 5;
+let fact = 1;
+for (let i = 1; i <= n1; i++) {
+  fact *= i;
+}
+console.log(fact);
+
+// ----------------------------------------
+// factor of n numbers
+
+// let ans = prompt("Enter a number");
+let ans = 4;
+
+if (ans === null || ans === undefined) {
+  console.log("cancelled");
+} else {
+  let n = Number(ans);
+  if (isNaN(n)) {
+    console.log("Invalid input");
+  } else {
+    if (n > 0) {
+      for (let i = 1; i <= n; i++) {
+        if (n % i === 0) {
+          console.log(i);
+        }
+      }
+    } else {
+      console.log("should be +ve and more than 0 ");
+    }
+  }
+}
+
+//  more optimized way to find factors of a number
+
+let a = 8;
+for (let i = 1; i <= Math.floor(a / 2); i++) {
+  if (a % i == 0) {
+    console.log(i);
+  }
+}
+console.log(a);
+
+// As any number divided by 2 would make the numbers above the divided outcome to be non-factor except the number itself.
+// ex: 8 , 8/2 = 4 , then all factor will be <= 4 and 8 itself(which is >=+- 8).
