a14a323e627427284dec8f8221089082dcb29fd3
Shweta Nigam <shwetanigam2106@gmail.com>
2025-11-29 12:38:26 +0530

    unary operator

--- 01_JS/01_JS_basics_logic/08_unary_operator.js
+++ 01_JS/01_JS_basics_logic/08_unary_operator.js
@@ -46,3 +46,19 @@ let j2 = 11
 let k2 = ++(j2++)  
 console.log(k2);  //  ❌SyntaxError: Invalid left-hand side expression in prefix operation
 // because here first (j2++) will be resolved to 12 and then it will become ++12 which is invalid(see above)
+
+
+
+// A unary operator is an operator that takes a single operand. Here are some of the most common unary operators in JavaScript:
+// Unary Plus (+): Converts its operand to a number. It's often used for explicit type conversion of strings or booleans to numbers.
+
+//     let strNum = "42";
+//     let num = +strNum; // num is 42 (a number)
+//     let boolVal = true;
+//     let numFromBool = +boolVal; // numFromBool is 1
+// Unary Negation (-): Converts its operand to a number and then negates it. 
+
+//     let positiveNum = 10;
+//     let negativeNum = -positiveNum; // negativeNum is -10
+//     let strNegative = "-5";
+//     let numFromStr = -strNegative; // numFromStr is 5
