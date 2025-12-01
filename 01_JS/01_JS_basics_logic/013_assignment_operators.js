bac1bf1164382ed0f61f2bddfd27b8d7665c9964
Shweta Nigam <shwetanigam2106@gmail.com>
2025-12-01 12:15:27 +0530

    assignment operators

---
+++ 01_JS/01_JS_basics_logic/013_assignment_operators.js
@@ -0,0 +1,17 @@
+// = assignment  operator
+// += means update the value and save it.
+// ex:-
+let a = 12;
+a += 3; // add 3 in a and update it.
+console.log(a); // 15
+a -= 11;
+console.log(a); // 4
+a *= 2;
+a /= 2;
+a %= 3;
+console.log(a);//1
+
+
+console.log(+5);     //allowed 
+console.log(++5);   // not allowed
+
