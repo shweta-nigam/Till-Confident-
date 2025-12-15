b31677f03b6d31c2144361ab3fc9ad6b459ae0b8
Shweta Nigam <shwetanigam2106@gmail.com>
2025-12-15 15:35:04 +0530

    learn DOM

---
+++ 01_JS/010_DOM/01_basic/01_practice.js
@@ -0,0 +1,11 @@
+const abcd = document.getElementById("abcd");
+console.log(abcd); // log does not show draggable tag sometimes
+console.dir(abcd); // so use console.dir()
+
+// note: getELementById and getELementsByCLassName is legacy code now.
+
+// querySelectorAll give :- collection of node - an array like structure.
+
+
+//----------------
+
