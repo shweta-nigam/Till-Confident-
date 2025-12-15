35e6252a63af9aa2bc742349c2a92c266227456a
Shweta Nigam <shwetanigam2106@gmail.com>
2025-12-15 15:34:51 +0530

    print prog notes

--- 01_JS/04_pattern_programming/04_practice.js
+++ 01_JS/04_pattern_programming/04_practice.js
@@ -27,3 +27,50 @@ for (let i = 1; i <= 5; i++) {
 
   console.log();
 }
+
+// 2. right angle triangle
+let n = 5
+for(let i = 1; i <= n; i++){
+    for(let j = 1; j<= n-i; j++){
+        process.stdout.write(" ")
+    }
+    for(let j = 1; j <=i; j++){
+        process.stdout.write("*")
+    }
+    console.log();
+    
+}
+
+// 3. x pattern
+let n2 = 5
+
+for(let i = 1 ; i<=n2; i++){
+    for(let j = 1; j <= n2 ; j++){
+        if(i === j || i + j === n2+1){
+            process.stdout.write("*")
+        }
+        else process.stdout.write(" ")
+    }
+    console.log();
+}
+// x pattern is only possible when n = odd numbers
+
+// *   *
+//  * * 
+//   *  
+//  * * 
+// *   *
+
+
+// 4. v pattern
+let a = 5
+
+for(let i = 1 ; i<= a; i++){
+    for(let j = 1; j <= 2*a -1 ; j++){
+        if(i === j || i + j === 2*a){
+            process.stdout.write("*")
+        }
+        else process.stdout.write(" ")
+    }
+    console.log();
+}
