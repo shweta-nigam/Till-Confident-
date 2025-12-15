194f190b7596b1a743a9a86202a8f5fc3e70d1d9
Shweta Nigam <shwetanigam2106@gmail.com>
2025-12-15 10:56:42 +0530

    find largest num problem (easy)

--- 01_JS/0100_JS_practice.js/04_loops/03_logic.js
+++ 01_JS/0100_JS_practice.js/04_loops/03_logic.js
@@ -3,52 +3,114 @@
 // 1. Count how many digits are in a number.
 //    Example: 4567 → 4 digits
 
-function countNum(n){
-
-    let count = 0
-    while(n>0){
-        let rem = n % 10
-        n = Math.floor(n/10)
-        count++
-    }
-    return count
+function countNum(n) {
+  let count = 0;
+  while (n > 0) {
+    let rem = n % 10;
+    n = Math.floor(n / 10);
+    count++;
+  }
+  return count;
 }
 console.log(countNum(3456));
 
+function countNum(n) {
+  // better than before
 
-
-function countNum(n){  // better than before 
-
-    let count = 0
-    while(n>0){
-        n = Math.floor(n/10)
-        count++
-    }
-    return count
+  let count = 0;
+  while (n > 0) {
+    n = Math.floor(n / 10);
+    count++;
+  }
+  return count;
 }
 console.log(countNum(3000));
-
 
 // 2. Reverse a number.
 //    Example: 1234 → 4321
 
-function reverseNum(n){
-    let num = 0
-   while(n>0){
-    let rem = n % 10 
-    n = Math.floor(n/10)
-  num = num*10 + rem
-   }
-   return num
+function reverseNum(n) {
+  let num = 0;
+  while (n > 0) {
+    let rem = n % 10;
+    n = Math.floor(n / 10);
+    num = num * 10 + rem;
+  }
+  return num;
 }
 console.log(reverseNum(1234));
-
 
 // 3. Find the product of digits of a number.
 //    Example: 235 → 2 * 3 * 5 = 30
 
+function productOfDigits(n) {
+  let product = 1;
+  while (n > 0) {
+    let rem = n % 10;
+    n = Math.floor(n / 10);
+    product *= rem;
+  }
+  return product;
+}
+
+console.log(productOfDigits(235));
+
 // 4. Find the largest digit in a number.
 //    Example: 9462 → 9
+
+//attempt 1  - works but not optimized
+function largestNum(n) {
+  // gives correct answer  - > but not good -> as array in unnecessary
+  let arr = [];
+  while (n > 0) {
+    let rem = n % 10;
+    n = Math.floor(n / 10);
+    arr.push(rem);
+  }
+  return Math.max(...arr);
+}
+console.log(largestNum(9462));
+
+//------------------------------  math.max() and array and spread operator
+let arr = [2, 3, 45, 6];
+console.log(...arr); // 2 3 45 6
+console.log(Math.max(...arr)); // 45
+
+console.log(Math.max([12, 34, 6, 4])); // NaN - Math.max() does not accept array.
+//---------------------------------
+
+// attempt 2   - failed
+function largestNum2(n) {
+  let lNum = 0;
+  while (n > 0) {
+    let rem = n % 10;
+    n = Math.floor(n / 10);
+
+    if (lNum > rem) {
+      return lNum;
+    }
+    lNum = rem;
+  }
+  return lNum;
+}
+console.log(largestNum2(1098689));
+console.log(largestNum2(11119));
+console.log(largestNum2(91111));
+
+// attempt 3          ---- good (finally)
+function largestNum3(n) {
+  let lNum = 0;
+  while (n > 0) {
+    let rem = n % 10;
+    n = Math.floor(n / 10);
+    if (rem > lNum) {
+        lNum = rem
+    }
+  }
+  return lNum
+}
+console.log(largestNum3(3457));
+
 
 // 5. Find the smallest digit in a number.
 //    Example: 9462 → 2
