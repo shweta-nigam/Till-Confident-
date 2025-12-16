489f0bd379282951cbd64fc7358d6fcaa7d47924
Shweta Nigam <shwetanigam2106@gmail.com>
2025-12-16 12:03:11 +0530

    practice simple logic questions

--- 01_JS/0100_JS_practice.js/04_loops/03_logic.js
+++ 01_JS/0100_JS_practice.js/04_loops/03_logic.js
@@ -163,18 +163,83 @@ console.log(palindrome("010")); //  logic may return false
 // This is a deeper conceptual bug.
 
 //b. check fails for input 0
-// when palindrom(0) then while(n>0) loop never run but 0 === Number("") → true
+// when palindrome(0) then while(n>0) loop never run but 0 === Number("") → true
 // This is accidentally correct, not logically correct. 
 
 
+//------------------------------------------------------------------------------------------------
 // 7. Print the multiplication table of a number.
 //    Example: 5 → 5×1 = 5 … 5×10 = 50
+
+function table(n){     // good
+    for(let i = 1; i <=10 ; i++){
+console.log( `${n} x ${i} = ${n * i}`); 
+    }
+}
+table(5);
+
+// now same result with return keyword
+
+function table2(n){     // good
+    let result = ""
+    for(let i = 1; i <=10 ; i++){
+result += `${n} x ${i} = ${n * i}` + "\n"; 
+    }
+    return result
+}
+console.log(table2(5));
+
 
 // 8. Count how many even and odd digits a number has.
 //    Example: 4826 → even digits = 4, odd digits = 0
 
+function find_Even_Odd(n){
+let oddCount = 0
+let evenCount = 0
+// if(n===0) return `even digits = ${evenCount++}`// it will log 0 and not 1 as It inserts 0, not 1. (post - increment)
+if(n===0) return `even digits = ${++evenCount}`
+while(n>0){
+    let rem = n%10
+    n = Math.floor(n/10);
+    if(rem % 2 === 0) {
+        evenCount++
+    } else {
+        oddCount++
+    }
+}
+return `even digits = ${evenCount}, odd digits = ${oddCount}`
+}
+console.log(find_Even_Odd(482670));
+console.log(find_Even_Odd(0));
+// Note: why post-increment with count variables and pre-increment when counting 0?
+// Because 0 count is the immediate return (needing updated value immediately).
+// and post-increments with other number count as they are not returning immediately.
+
+//--------------------------------------------------------------------
 // 9. Find factorial of a number using a while loop.
 //    Example: 5 → 120
 
+function factorial(n){
+    let result = 1
+    // for(let i = n; i > 0 ; i--){          // both works
+    //         result *= i
+    // }
+    for(let i = 1; i <= n ; i++){
+            result *= i
+    }
+    return result
+}
+console.log(factorial(5));
+
+
 // 10. Find the sum of numbers from 1 to n using a while loop.
 //     Example: n = 5 → 1+2+3+4+5 = 15
+
+function sum(n){
+    let sum = 0
+    for(let i = 1; i <= n; i++){
+        sum += i
+    }
+    return sum
+}
+console.log(sum(5));
