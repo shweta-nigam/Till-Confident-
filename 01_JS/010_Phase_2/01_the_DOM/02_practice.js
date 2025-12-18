3ba8bddefddcd6789a0853857bb3ec7074664f2c
Shweta Nigam <shwetanigam2106@gmail.com>
2025-12-18 12:01:40 +0530

    learn DOM

--- 01_JS/010_Phase_2/01_the_DOM/02_practice.js
+++ 01_JS/010_Phase_2/01_the_DOM/02_practice.js
@@ -1,2 +1,19 @@
 //dynamic DOM manipulation: createELement , appendChild, removeChild, prepend
 
+let h1 = document.createElement("h1");      // this creates blank h1 
+h1.textContent = "Hi there!"
+
+console.log(h1);
+document.body.append(h1)        // Adds as the last child INSIDE body
+document.body.prepend(h1)      // Adds as the first child INSIDE body
+
+//or
+document.querySelector("body").append(h1)
+document.querySelector("body").prepend(h1)
+
+// note: A DOM node can exist in only one location at a time
+// So:
+// append / prepend move elements
+// They do NOT clone
+
+//---------------------------------------------------------------
