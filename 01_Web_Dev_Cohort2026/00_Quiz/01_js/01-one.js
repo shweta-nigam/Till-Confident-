648a6bdc15b20b68329432c966cc88be461cbe98
Shweta Nigam <shwetanigam2106@gmail.com>
2026-03-10 14:04:10 +0530

    notes on quiz

---
+++ 01_Web_Dev_Cohort2026/00_Quiz/01_js/01-one.js
@@ -0,0 +1,21 @@
+
+// only correct ans
+
+//1
+// Closures occur when ________
+// - Inner function accesses outer function variables
+// - function remembers its lexical env
+
+// 2
+// Q- which statements about optional chaining(?.) are correct?
+// - prevents runtime error when accessing nested properties
+// - returns undefined if property does not exists
+// - works with function calls  ------> (search more)
+
+//3.
+// Q- which statement is/are true in the given options?
+// - JSON supports strings,numbers ,arrays, object
+// - JSON does not support functions,comments,undefined etc.
+
+//4. 
+// map, filter, slice does not mutates array
